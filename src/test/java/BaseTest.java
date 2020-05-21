9b049034d9416b92ce181e845c06b98574353073
d_arg <d_argunov@bk.ru>
2020-05-21 10:52:56 +0300

    fix

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -8,17 +8,15 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
-    String CHROMEDRIVER_PATH = Constants.CHROMEDRIVER_PATH;
-    String BASE_URL = Constants.BASE_URL;
     WebDriver driver;
     BasePage basePage;
 
     @BeforeClass
     public void before() {
-        System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH);
+        System.setProperty("webdriver.chrome.driver", Constants.CHROMEDRIVER_PATH);
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(BASE_URL);
+        driver.get(Constants.BASE_URL);
         basePage = new BasePage(driver);
         basePage.login(Constants.EMAIL, Constants.PASSWORD);
     }
